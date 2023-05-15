77f646eb7b713f8f02b4703c3fff9d0d7c6972a0
ididdidi <ididdidi.dev@gmail.com>
2023-05-15 21:58:52 +0300

    ADD: custom Attributes

---
+++ Attributes/LabelProperties/LabelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityExtended
+{
+    /// <summary>
+    /// Attribute for custom label
+    /// Allows you to change the attribute name
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class LabelAttribute : PropertyAttribute
+    {
+        // Label text
+        public string text;
+
+        /// <summary>
+        /// Requiring implementation of the <see cref="LabelAttribute"/> interface.
+        /// </summary>
+        /// <param name="label">Label.</param>
+        public LabelAttribute(string label) => this.text = label;
+    }
+}
