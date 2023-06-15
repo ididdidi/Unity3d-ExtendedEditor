1df1402c7218b3753c7a11c1b9427a49fae20061
ididdidi <ididdidi.dev@gmail.com>
2023-06-15 17:19:25 +0300

    Moved ChecValues methods to separate file

--- Editor/ExtendedEditorGUI.cs
+++ Editor/ExtendedEditorGUI.cs
@@ -8,8 +8,6 @@ namespace UnityExtended
     /// </summary>
     public static partial class ExtendedEditorGUI
     {
-
-
         /// <summary>
         /// Provides a new rect on top of the existing one.
         /// </summary>
@@ -60,60 +58,5 @@ namespace UnityExtended
             // Display message
             EditorGUILayout.LabelField(label, style);
         }
-
-        /// <summary>
-        /// Checks the validity of the dragged objects
-        /// </summary>
-        /// <param name="position"><see cref="Rect"/> fields to activate validation</param>
-        /// <param name="requiredType">Required <see cref="System.Type"/> of reference being checked</param>
-        public static void ChecDragAndDrops(Rect position, params System.Type[] requiredTypes)
-        {
-            // If the cursor is in the area of the rendered field
-            if (position.Contains(Event.current.mousePosition))
-            {
-                // Iterate over all draggable references
-                foreach (var @object in DragAndDrop.objectReferences)
-                {
-                    // If we do not find the required type
-                    if (!IsValidObject(@object, requiredTypes))
-                    {
-                        // Disable drag and drop
-                        DragAndDrop.visualMode = DragAndDropVisualMode.Rejected;
-                        break;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Checks if an reference matches the required type
-        /// </summary>
-        /// <param name="object">Checked reference</param>
-        /// <param name="requiredType">Required <see cref="System.Type"/> of reference being checked</param>
-        /// <returns></returns>
-        private static bool IsValidObject(Object @object, params System.Type[] requiredTypes)
-        {
-            foreach(var type in requiredTypes)
-            {
-                // If the object is a GameObject
-                if (@object is GameObject go)
-                {
-                    // Check if it has a component of the required type and return result
-                    if (go.GetComponent(type) != null) { return true; }
-                }
-
-                // Check the reference itself for compliance with the required type
-                if (type.IsAssignableFrom(@object.GetType())) { return true; }
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Checks a previously added object for compliance
-        /// </summary>
-        /// <param name="object">Checked object</param>
-        /// <param name="requiredType">Required <see cref="System.Type"/> of reference being checked</param>
-        /// <returns></returns>
-        public static Object ChecValues(Object @object, params System.Type[] requiredTypes) => @object != null && IsValidObject(@object, requiredTypes) ? @object : null;
     }
 }
